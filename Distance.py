b26fe3545398f1839498644ff5d439ad956b5bbe
Jack Madden <46815674+Jack-Madden@users.noreply.github.com>
2019-04-04 10:37:56 +0100

    Update Distance.py

--- Distance.py
+++ Distance.py
@@ -1,14 +1,15 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-import pythonosc
 
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
 
 sensor = DistanceSensor(echo=17, trigger=4)
-#sender = udp_client.SimpleUDPClient('127.0.0.1', 4559)
+sender = udp_client.SimpleUDPClient('127.0.0.1', 4559)
 
 while True:
     pitch = round(sensor.distance * 100 + 30)
-#/    sender.send_message('/play_this', pitch)
+    sender.send_message('/play_this', pitch)
     print(pitch)
     sleep(.1)
